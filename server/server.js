d58792f9b24f263cf36a724a3ea119d884e697c8
Paras Teli <parasteli2004@gmail.com>
2024-01-27 01:25:29 +0530

    connection with mongoDB database and setting up enviroment variables

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -6,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.bold.yellow
+  );
 });
