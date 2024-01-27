04007ce80ac5b2ee05babeb21bc9dacdbf0af3c4
Paras Teli <parasteli2004@gmail.com>
2024-01-27 16:19:31 +0530

    Adding Error middlewares

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +18,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(
