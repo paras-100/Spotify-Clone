19619589a9d2f3bc4440a6320035a28230f4d394
Paras Teli <parasteli2004@gmail.com>
2025-03-04 17:36:28 +0530

    changes

---
+++ client/src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { userSlice } from "./slices/userSlices";
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
+
+export default store;
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,10 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
-import "@fontsource/zen-kaku-gothic-new/300.css";
-import "@fontsource/zen-kaku-gothic-new/400.css";
-import "@fontsource/zen-kaku-gothic-new/500.css";
-import "@fontsource/zen-kaku-gothic-new/700.css";
-import "@fontsource/zen-kaku-gothic-new/900.css";
+// import "@fontsource/zen-kaku-gothic-new/300.css";
+// import "@fontsource/zen-kaku-gothic-new/400.css";
+// import "@fontsource/zen-kaku-gothic-new/500.css";
+// import "@fontsource/zen-kaku-gothic-new/700.css";
+// import "@fontsource/zen-kaku-gothic-new/900.css";
 
 const theme = extendTheme({
   fonts: {
